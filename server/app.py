2e8935312acf349b7283db151bff134647961e6b
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-03-14 21:39:08 -0500

    Flask API files

---
+++ server/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#Init app
+app=Flask(__name__)
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'db.sqllite')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init DB
+db =SQLAlchemy(app)
+
+
+#init marshamllow
+ma=Marshmallow(app)
+
+
+#Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
