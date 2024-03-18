32b617d2377b8c22e78fa578c66b618b57858a0e
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-03-18 17:13:55 -0500

    Add routes

--- client/grade-app/src/app/app-routing.module.ts
+++ client/grade-app/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent},
+  { path: 'register', component: RegistrationPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
