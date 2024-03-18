6811aea1f71c171d93fe7f3e7019e5cda894fc09
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-03-18 17:50:53 -0500

    add home page

--- client/grade-app/src/app/app-routing.module.ts
+++ client/grade-app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', component: LoginPageComponent},
   { path: 'register', component: RegistrationPageComponent},
+  {path: 'home', component:HomePageComponent}
 ];
 
 @NgModule({
