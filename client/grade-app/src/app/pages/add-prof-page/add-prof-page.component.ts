7e8b42c9b2c2a757c81a7d9e14add8f525dbedf1
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-03-27 22:23:33 -0500

    Able to add prof

--- client/grade-app/src/app/pages/add-prof-page/add-prof-page.component.ts
+++ client/grade-app/src/app/pages/add-prof-page/add-prof-page.component.ts
@@ -1,10 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-prof-page',
   templateUrl: './add-prof-page.component.html',
-  styleUrl: './add-prof-page.component.scss'
+  styleUrl: './add-prof-page.component.scss',
 })
 export class AddProfPageComponent {
+  myForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      professorName: ['', Validators.required],
+    });
+  }
+
+  async onSubmit() {
+    if (this.myForm.valid) {
+      const newProf = {
+        lastName: this.myForm.value.professorName,
+      };
+
+      try {
+        const response = await this.http
+          .post<any>('http://localhost:3000/create-prof', newProf)
+          .toPromise();
+        this.router.navigate(['/']);
+      } catch (error) {}
+    } else {
+    }
+  }
 }
