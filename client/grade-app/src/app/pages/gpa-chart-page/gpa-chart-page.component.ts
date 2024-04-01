1b28ee8765f68c40b12cedc2b2b9921cbc750836
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-03-31 20:54:25 -0500

    Add service for GPA analytics

--- client/grade-app/src/app/pages/gpa-chart-page/gpa-chart-page.component.ts
+++ client/grade-app/src/app/pages/gpa-chart-page/gpa-chart-page.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gpa-chart-page',
   templateUrl: './gpa-chart-page.component.html',
   styleUrl: './gpa-chart-page.component.scss'
 })
-export class GPAChartPageComponent {
+export class GPAChartPageComponent implements OnInit {
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
